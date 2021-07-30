75b1444b17a0d95534d7992eaf4f7c631167530f
Chuyue <chuyueluo@gmail.com>
2021-07-30 21:32:10 +0100

    test

--- codeclone/test_codeclone.py
+++ codeclone/test_codeclone.py
@@ -44,7 +44,7 @@ infercode = InferCodeClient(language="c")
 infercode.init_from_config()
 
 #vectors = infercode.encode([code1, code2])
-vectors = infercode.encode(["char c = 'c'", "int i = 0"])
+vectors = infercode.encode(["return 1", "int i = 0"])
 
 if cosine_similarity(vectors[0], vectors[1]) > 0.8:
     print("code clone")
