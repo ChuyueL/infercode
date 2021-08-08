03b263f3ec6ea8ef9e3aa2c3ea2419d74cfaa394
Chuyue <chuyueluo@gmail.com>
2021-08-08 23:15:53 +0100

    changing indentation

--- infercode/data_utils/language_util.py
+++ infercode/data_utils/language_util.py
@@ -11,29 +11,29 @@ import numpy as np
 from bidict import bidict
 
 class LanguageUtil():
-
     def __init__(self):
         self.languages = bidict({
-                "java": ".java", 
-                "c": ".c", 
-                "c_sharp": ".cs", 
-                "cpp": ".cpp",
-                "bash": ".sh",
-                "go": ".go",
-                "javascript": ".js",
-                "lua": ".lua",
-                "php": ".php",
-                "python": ".py",
-                "ruby": ".rb",
-                "rust": ".rs",
-                "scala": ".scala",
-                "kotlin": ".kt",
-                "solidity": ".sol",
-                "html": ".html",
-                "css": ".css",
-                "haskell": ".hs",
-                "r": ".r"})
+            "java": ".java", 
+            "c": ".c", 
+            "c_sharp": ".cs", 
+            "cpp": ".cpp",
+            "bash": ".sh",
+            "go": ".go",
+            "javascript": ".js",
+            "lua": ".lua",
+            "php": ".php",
+            "python": ".py",
+            "ruby": ".rb",
+            "rust": ".rs",
+            "scala": ".scala",
+            "kotlin": ".kt",
+            "solidity": ".sol",
+            "html": ".html",
+            "css": ".css",
+            "haskell": ".hs",
+            "r": ".r"})
     
+
     def get_language_by_file_extension(self, extension):
         return self.languages.inverse[extension]
         
